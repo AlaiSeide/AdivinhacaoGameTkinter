8f1c6553472038ae6ff9aa65b1bd87e9cb754a87
Alai Seide <154024184+AlaiSeide@users.noreply.github.com>
2023-12-20 21:04:02 +0100

    Jogo de Adivinhação em Python com Interface Gráfica
    Descrição:
    Este simples jogo de adivinhação em Python utiliza a biblioteca Tkinter para criar uma interface gráfica interativa. O jogador insere um número através da interface, clica no botão "Chutar" e recebe feedback se o chute foi maior, menor ou igual ao valor aleatório gerado. Uma ótima introdução ao desenvolvimento de jogos com Python e Tkinter!

---
+++ adivinhacao-game-tkinter.py
@@ -0,0 +1,34 @@
+import random
+import tkinter
+
+# Criando a janela principal
+janela = tkinter.Tk()
+janela.title("Jogo de Adivinhação")
+
+# Gerando um valor aleatório entre 1 e 5
+valor_aleatorio = random.randint(1, 5)
+
+# Função para lidar com o chute do jogador
+def Chute():
+    chute = int(entry.get())
+    if chute > valor_aleatorio:
+        resultado.config(text='Chute é maior que o valor gerado')
+    elif chute < valor_aleatorio:
+        resultado.config(text='Chute é menor que o valor gerado')
+    else:
+        resultado.config(text='PARABÉNS! Você acertou.')
+
+# Configurando a entrada do usuário
+entry = tkinter.Entry(janela)
+entry.pack()
+
+# Configurando o botão de chute
+botao = tkinter.Button(janela, text="Chutar", command=Chute)
+botao.pack()
+
+# Configurando a exibição do resultado
+resultado = tkinter.Label(janela, text=" ")
+resultado.pack()
+
+# Iniciando o loop da interface gráfica
+janela.mainloop()
